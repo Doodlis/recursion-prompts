80c57de1aeffe6ec13d616a79a64e5d9218102dd
Doodlis <kornelija.janusaite@gmail.com>
2020-09-23 18:22:24 -0500

    DONE: 19-24.

--- src/recursion.js
+++ src/recursion.js
@@ -320,17 +320,53 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+    if  (n === 0) {
+        return [];
+    }
+    var result; 
+    if (n % 3 === 0 && n % 5 === 0) {
+        result ='FizzBuzz';
+    } else if (n % 3 === 0) {
+        result = 'Fizz';
+    } else if (n % 5 === 0) {
+        result = 'Buzz';
+    } else {
+        result = n.toString();
+    }
+    return [...fizzBuzz(n-1), result];
+
 };
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+
+    if (array.length === 0) {
+        return 0;
+    }
+    if (array[0] === value) {
+        array.shift();
+        return 1 + countOccurrence(array, value);
+    } else {
+        array.shift()
+        return 0 + countOccurrence(array, value);
+    }
+
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+    if (array.length === 0) {
+        return [];
+    }
+
+    var index = 0;
+    if (index <= array.length - 1) {
+        var result = callback(array[index]);
+        return   [result, ...rMap(array.slice(index+1), callback)];
+    }
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
@@ -338,6 +374,17 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+    var count = 0;
+    for (var keys in obj) {
+        if (keys === key) {
+            count++
+        }
+        var value = obj[keys];
+        if (typeof(value) === 'object') {
+            count += countKeysInObj(value, key)
+        } 
+    }
+    return count;
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -345,11 +392,33 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+    var count = 0;
+    for (var keys in obj) {
+        if (obj[keys] === value){
+        count++;
+        }
+        if (typeof(obj[keys]) === 'object') {
+            count += countValuesInObj(obj[keys], value);
+        }
+    }
+    return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+    
+    for (var keys in obj) {
+        if (keys === oldKey) {
+            obj[newKey] = obj[oldKey];
+            delete obj[oldKey];   
+        }
+        var value = obj[keys];
+        if (typeof(value) === 'object') {
+            replaceKeysInObj(value, oldKey, newKey);
+        }   
+    }
+    return obj
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
